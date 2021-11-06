f8931cd18648cb8ac4bb16186139a9618cb32b90
viveknimbolkar <viveknimbolkar50@gmail.com>
2021-11-06 08:46:10 +0530

    add credentials and db file

--- SwiftBankDB.sql
+++ SwiftBankDB.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: localhost
--- Generation Time: Apr 07, 2021 at 07:58 PM
+-- Generation Time: Nov 06, 2021 at 04:14 AM
 -- Server version: 10.4.17-MariaDB
 -- PHP Version: 8.0.2
 
@@ -44,107 +44,16 @@ CREATE TABLE `customerdata` (
   `dob` varchar(20) NOT NULL,
   `balance` varchar(50) NOT NULL,
   `pincode` varchar(10) NOT NULL,
-  `customer_img` varchar(50) NOT NULL,
   `gender` varchar(10) NOT NULL,
   `timestamp` date NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
--- --------------------------------------------------------
-
 --
--- Table structure for table `customer_contacts`
+-- Dumping data for table `customerdata`
 --
 
-CREATE TABLE `customer_contacts` (
-  `ID` int(10) NOT NULL,
-  `fullname` varchar(50) NOT NULL,
-  `email_address` varchar(30) NOT NULL,
-  `subject` varchar(50) NOT NULL,
-  `message` varchar(300) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
-
--- --------------------------------------------------------
-
---
--- Table structure for table `employeedata`
---
-
-CREATE TABLE `employeedata` (
-  `ID` int(5) NOT NULL,
-  `first_name` varchar(30) NOT NULL,
-  `father_name` varchar(30) NOT NULL,
-  `last_name` varchar(30) NOT NULL,
-  `email_address` varchar(30) NOT NULL,
-  `pass` varchar(30) NOT NULL,
-  `mobile_no` varchar(20) NOT NULL,
-  `aadhar_no` varchar(20) NOT NULL,
-  `emp_id` varchar(10) NOT NULL,
-  `dob` varchar(10) NOT NULL,
-  `qualification` varchar(50) NOT NULL,
-  `address` varchar(100) NOT NULL,
-  `city` varchar(20) NOT NULL,
-  `taluka` varchar(20) NOT NULL,
-  `district` varchar(20) NOT NULL,
-  `state` varchar(20) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
-
--- --------------------------------------------------------
-
---
--- Table structure for table `manager`
---
-
-CREATE TABLE `manager` (
-  `ID` int(5) NOT NULL,
-  `name` varchar(50) NOT NULL,
-  `email` varchar(30) NOT NULL,
-  `pass` varchar(50) NOT NULL,
-  `dob` varchar(10) NOT NULL,
-  `address` varchar(100) NOT NULL,
-  `mobile` varchar(30) NOT NULL,
-  `aadhar_no` varchar(30) NOT NULL,
-  `qualification` varchar(50) NOT NULL,
-  `age` varchar(3) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
-
--- --------------------------------------------------------
-
---
--- Table structure for table `subscribers`
---
-
-CREATE TABLE `subscribers` (
-  `id` int(10) NOT NULL,
-  `subscriber_email` varchar(50) NOT NULL,
-  `timestamp` date NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
-
--- --------------------------------------------------------
-
---
--- Table structure for table `transaction_history`
---
-
-CREATE TABLE `transaction_history` (
-  `id` int(11) NOT NULL,
-  `from_account` varchar(30) NOT NULL,
-  `to_account` varchar(30) NOT NULL,
-  `transfer_amount` varchar(30) NOT NULL,
-  `date` date NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
-
--- --------------------------------------------------------
-
---
--- Table structure for table `visitors_detail`
---
-
-CREATE TABLE `visitors_detail` (
-  `id` int(50) NOT NULL,
-  `user_ip` varchar(50) NOT NULL,
-  `user_agent` varchar(240) NOT NULL,
-  `access_time` timestamp(6) NOT NULL DEFAULT current_timestamp(6) ON UPDATE current_timestamp(6)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+INSERT INTO `customerdata` (`ID`, `first_name`, `father_name`, `last_name`, `email_address`, `mobile_no`, `account_no`, `aadhar_no`, `address`, `city`, `taluka`, `district`, `state`, `dob`, `balance`, `pincode`, `gender`, `timestamp`) VALUES
+(111, 'Firstname', 'Fathername', 'Lastname', 'email@gmail.com', '8754213265', '1327765670', '986532215487', 'near small town 45, amravati', 'Pnacha', 'Kolkata', 'Delhi', 'Jharkhand', '2021-08-12', '200', '986532', 'Male', '2021-08-29');
 
 --
 -- Indexes for dumped tables
@@ -157,36 +66,6 @@ ALTER TABLE `customerdata`
   ADD PRIMARY KEY (`ID`);
 
 --
--- Indexes for table `customer_contacts`
---
-ALTER TABLE `customer_contacts`
-  ADD PRIMARY KEY (`ID`);
-
---
--- Indexes for table `employeedata`
---
-ALTER TABLE `employeedata`
-  ADD PRIMARY KEY (`ID`);
-
---
--- Indexes for table `subscribers`
---
-ALTER TABLE `subscribers`
-  ADD PRIMARY KEY (`id`);
-
---
--- Indexes for table `transaction_history`
---
-ALTER TABLE `transaction_history`
-  ADD PRIMARY KEY (`id`);
-
---
--- Indexes for table `visitors_detail`
---
-ALTER TABLE `visitors_detail`
-  ADD PRIMARY KEY (`id`);
-
---
 -- AUTO_INCREMENT for dumped tables
 --
 
@@ -194,37 +73,7 @@ ALTER TABLE `visitors_detail`
 -- AUTO_INCREMENT for table `customerdata`
 --
 ALTER TABLE `customerdata`
-  MODIFY `ID` int(5) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=103;
-
---
--- AUTO_INCREMENT for table `customer_contacts`
---
-ALTER TABLE `customer_contacts`
-  MODIFY `ID` int(10) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
-
---
--- AUTO_INCREMENT for table `employeedata`
---
-ALTER TABLE `employeedata`
-  MODIFY `ID` int(5) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=17;
-
---
--- AUTO_INCREMENT for table `subscribers`
---
-ALTER TABLE `subscribers`
-  MODIFY `id` int(10) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
-
---
--- AUTO_INCREMENT for table `transaction_history`
---
-ALTER TABLE `transaction_history`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=11;
-
---
--- AUTO_INCREMENT for table `visitors_detail`
---
-ALTER TABLE `visitors_detail`
-  MODIFY `id` int(50) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=52;
+  MODIFY `ID` int(5) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=113;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
