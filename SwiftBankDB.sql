e35a985c2ffd8acd10cafc8e2fcff3744fae388a
viveknimbolkar <viveknimbolkar50@gmail.com>
2021-12-28 13:34:28 +0530

    fix: update sql file

--- SwiftBankDB.sql
+++ SwiftBankDB.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: localhost
--- Generation Time: Nov 06, 2021 at 04:14 AM
+-- Generation Time: Dec 28, 2021 at 09:01 AM
 -- Server version: 10.4.17-MariaDB
 -- PHP Version: 8.0.2
 
@@ -55,6 +55,133 @@ CREATE TABLE `customerdata` (
 INSERT INTO `customerdata` (`ID`, `first_name`, `father_name`, `last_name`, `email_address`, `mobile_no`, `account_no`, `aadhar_no`, `address`, `city`, `taluka`, `district`, `state`, `dob`, `balance`, `pincode`, `gender`, `timestamp`) VALUES
 (111, 'Firstname', 'Fathername', 'Lastname', 'email@gmail.com', '8754213265', '1327765670', '986532215487', 'near small town 45, amravati', 'Pnacha', 'Kolkata', 'Delhi', 'Jharkhand', '2021-08-12', '200', '986532', 'Male', '2021-08-29');
 
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `customer_contacts`
+--
+
+CREATE TABLE `customer_contacts` (
+  `ID` int(10) NOT NULL,
+  `fullname` varchar(50) NOT NULL,
+  `email_address` varchar(30) NOT NULL,
+  `subject` varchar(50) NOT NULL,
+  `message` varchar(300) NOT NULL,
+  `time` timestamp(6) NOT NULL DEFAULT current_timestamp(6) ON UPDATE current_timestamp(6)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `employeedata`
+--
+
+CREATE TABLE `employeedata` (
+  `ID` int(5) NOT NULL,
+  `first_name` varchar(30) NOT NULL,
+  `father_name` varchar(30) NOT NULL,
+  `last_name` varchar(30) NOT NULL,
+  `email_address` varchar(100) NOT NULL,
+  `pass` varchar(255) NOT NULL,
+  `mobile_no` varchar(20) NOT NULL,
+  `aadhar_no` varchar(20) NOT NULL,
+  `emp_id` varchar(10) NOT NULL,
+  `dob` varchar(10) NOT NULL,
+  `qualification` varchar(255) NOT NULL,
+  `address` varchar(255) NOT NULL,
+  `city` varchar(100) NOT NULL,
+  `taluka` varchar(100) NOT NULL,
+  `district` varchar(100) NOT NULL,
+  `state` varchar(20) NOT NULL,
+  `time` timestamp(6) NOT NULL DEFAULT current_timestamp(6) ON UPDATE current_timestamp(6)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `employeedata`
+--
+
+INSERT INTO `employeedata` (`ID`, `first_name`, `father_name`, `last_name`, `email_address`, `pass`, `mobile_no`, `aadhar_no`, `emp_id`, `dob`, `qualification`, `address`, `city`, `taluka`, `district`, `state`, `time`) VALUES
+(29, 'John', 'Harish', 'Doe', 'employee@gmail.com', '$2y$10$cE5WQSixDp09ODpwe1o/keN4FBuOnmAOoEsiUqDw0IxSo6qm6inya', '6536298745', '326598784564', '9869', '2021-11-18', 'B.E.', 'at post delhi india', 'Kolkata', 'Delhi', 'UP', 'Jharkhand', '2021-11-06 02:57:22.000000');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `manager`
+--
+
+CREATE TABLE `manager` (
+  `ID` int(5) NOT NULL,
+  `name` varchar(50) NOT NULL,
+  `email` varchar(100) NOT NULL,
+  `pass` varchar(255) NOT NULL,
+  `dob` date NOT NULL,
+  `address` varchar(255) NOT NULL,
+  `mobile` varchar(30) NOT NULL,
+  `aadhar_no` varchar(30) NOT NULL,
+  `qualification` varchar(255) NOT NULL,
+  `age` varchar(3) NOT NULL,
+  `time` timestamp(6) NOT NULL DEFAULT current_timestamp(6) ON UPDATE current_timestamp(6)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `manager`
+--
+
+INSERT INTO `manager` (`ID`, `name`, `email`, `pass`, `dob`, `address`, `mobile`, `aadhar_no`, `qualification`, `age`, `time`) VALUES
+(1, 'Martin Garrix', 'manager@gmail.com', 'Pass@123', '2021-09-16', '56, At Servt Street, Mumbai', '3265988754', '741258963214', 'B.Tech', '32', '2021-11-06 03:07:53.714378');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `subscribers`
+--
+
+CREATE TABLE `subscribers` (
+  `id` int(10) NOT NULL,
+  `subscriber_email` varchar(50) NOT NULL,
+  `timestamp` date NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `transaction_history`
+--
+
+CREATE TABLE `transaction_history` (
+  `id` int(11) NOT NULL,
+  `from_account` varchar(30) NOT NULL,
+  `to_account` varchar(30) NOT NULL,
+  `transfer_amount` varchar(30) NOT NULL,
+  `date` date NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `user_otp`
+--
+
+CREATE TABLE `user_otp` (
+  `id` int(100) NOT NULL,
+  `email_address` varchar(50) NOT NULL,
+  `otp` varchar(255) NOT NULL,
+  `otp_expire_time` int(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `visitors_detail`
+--
+
+CREATE TABLE `visitors_detail` (
+  `id` int(50) NOT NULL,
+  `user_ip` varchar(50) NOT NULL,
+  `user_agent` varchar(240) NOT NULL,
+  `access_time` timestamp(6) NOT NULL DEFAULT current_timestamp(6) ON UPDATE current_timestamp(6)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
 --
 -- Indexes for dumped tables
 --
@@ -66,6 +193,48 @@ ALTER TABLE `customerdata`
   ADD PRIMARY KEY (`ID`);
 
 --
+-- Indexes for table `customer_contacts`
+--
+ALTER TABLE `customer_contacts`
+  ADD PRIMARY KEY (`ID`);
+
+--
+-- Indexes for table `employeedata`
+--
+ALTER TABLE `employeedata`
+  ADD PRIMARY KEY (`ID`);
+
+--
+-- Indexes for table `manager`
+--
+ALTER TABLE `manager`
+  ADD PRIMARY KEY (`ID`);
+
+--
+-- Indexes for table `subscribers`
+--
+ALTER TABLE `subscribers`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `transaction_history`
+--
+ALTER TABLE `transaction_history`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `user_otp`
+--
+ALTER TABLE `user_otp`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `visitors_detail`
+--
+ALTER TABLE `visitors_detail`
+  ADD PRIMARY KEY (`id`);
+
+--
 -- AUTO_INCREMENT for dumped tables
 --
 
@@ -74,6 +243,48 @@ ALTER TABLE `customerdata`
 --
 ALTER TABLE `customerdata`
   MODIFY `ID` int(5) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=113;
+
+--
+-- AUTO_INCREMENT for table `customer_contacts`
+--
+ALTER TABLE `customer_contacts`
+  MODIFY `ID` int(10) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=9;
+
+--
+-- AUTO_INCREMENT for table `employeedata`
+--
+ALTER TABLE `employeedata`
+  MODIFY `ID` int(5) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=30;
+
+--
+-- AUTO_INCREMENT for table `manager`
+--
+ALTER TABLE `manager`
+  MODIFY `ID` int(5) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+
+--
+-- AUTO_INCREMENT for table `subscribers`
+--
+ALTER TABLE `subscribers`
+  MODIFY `id` int(10) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
+
+--
+-- AUTO_INCREMENT for table `transaction_history`
+--
+ALTER TABLE `transaction_history`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=13;
+
+--
+-- AUTO_INCREMENT for table `user_otp`
+--
+ALTER TABLE `user_otp`
+  MODIFY `id` int(100) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=47;
+
+--
+-- AUTO_INCREMENT for table `visitors_detail`
+--
+ALTER TABLE `visitors_detail`
+  MODIFY `id` int(50) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=189;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
